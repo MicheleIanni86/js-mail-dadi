3458f2bb253f9d5c5f527547e966fde58c81f63c
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-13 11:22:10 +0100

    feat: add JavaScript Exercize

--- MAIL/JS/main.js
+++ MAIL/JS/main.js
@@ -1,1 +1,19 @@
-const resultElement = document.getElementById('result'); 
+// const resultElement = document.getElementById('result');
+const emailLogin = prompt('Inserisci la tua mail');
+
+const emailOk = ['michele@gmail.com', 'tiziano@gmail.com', 'mattia@gmail.com', 'luigi@gmail.com'];
+
+let result = document.getElementById('result');
+
+
+for (let i = 0; i < emailOk.length; i++) {
+
+    if (emailLogin == emailOk[i]) {
+        result.innerHTML = "Accesso Consentito";
+        break;
+    } else {
+        result.innerHTML = "Accesso Negato";
+    }
+
+}
+
