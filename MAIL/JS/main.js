17972001baeff9e6ee0f2687d8b3a11eee36f13f
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-15 00:09:16 +0100

    feat: Add Button with "addEventListener" at EmailExercize

--- MAIL/JS/main.js
+++ MAIL/JS/main.js
@@ -1,19 +1,33 @@
 // const resultElement = document.getElementById('result');
-const emailLogin = prompt('Inserisci la tua mail');
+const emailLogin = document.getElementById('emailLogin');
+const buttonSendElement = document.getElementById('buttonSend');
+
+
 
 const emailOk = ['michele@gmail.com', 'tiziano@gmail.com', 'mattia@gmail.com', 'luigi@gmail.com'];
+console.log(emailOk);
 
 let result = document.getElementById('result');
 
 
-for (let i = 0; i < emailOk.length; i++) {
+buttonSendElement.addEventListener('click', function () {
 
-    if (emailLogin == emailOk[i]) {
-        result.innerHTML = "Accesso Consentito";
-        break;
-    } else {
-        result.innerHTML = "Accesso Negato";
+    for (let i = 0; i < emailOk.length; i++) {
+
+        if (emailLogin.value == '') {
+            alert('Inserire mail per continuare');
+            break;
+
+        } else if (emailLogin.value == emailOk[i]) {
+            result.innerHTML = "Accesso Consentito";
+
+            break;
+
+        } else {
+            result.innerHTML = "Accesso Negato";
+
+        }
+
     }
-
-}
+});
 
